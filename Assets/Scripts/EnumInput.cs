31a236699dc3467700e36b14effe1a68959fc735
Garret Sugarbaker <gsugarbaker@gmail.com>
2024-01-26 22:19:18 -0700

    add movement enum

---
+++ Assets/Scripts/EnumInput.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnumInput
+{
+    Left,
+    Right,
+    Up,
+    Down,
+    LeftTrigger,
+    RightTrigger,
+    LeftBumper,
+    RightBumper,
+    A,
+    B,
+    X,
+    Y
+}
